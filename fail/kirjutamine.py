dfacb4d4aa54bf88b072add997994d4448711347
Meelis Mets <meelis.mets@elisa.ee>
2022-10-17 13:28:39 +0300

    failist ja veebist lugemine

--- fail/kirjutamine.py
+++ fail/kirjutamine.py
@@ -2,7 +2,7 @@ nimi = input("Palun sisesta oma nimi: ")
 vanus = input("vanus: ")
 aadress = input("aadress: ")
 
-fail = open("kirjutatud.txt", "w")
+fail = open("kirjutatud.txt", "a")
 fail.write(nimi + "\n")
 fail.write(vanus + "\n")
 fail.write(aadress + "\n")
--- fail/lugemine.py
+++ fail/lugemine.py
@@ -1,4 +1,14 @@
-fail = open("lugemiseks.txt", encoding="UTF-8")
-for rida in fail:
-    print("Lugesin sellise rea: " + rida)
+fail = open("kirjutatud.txt", encoding="UTF-8")
+#loeme faili
+loetud_fail = fail.read()
+
 fail.close()
+#tükeldame faili
+tykeldatud_fail = loetud_fail.split("\n")
+
+ridade_arv = len(tykeldatud_fail) - 1
+
+for nr in range(0, ridade_arv, 3):
+    print("Nimi: " + tykeldatud_fail[nr])
+    print("Vanus: " + tykeldatud_fail[nr])
+
